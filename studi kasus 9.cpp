1904bd5a59c9d31413b90eb622b7ffbfc8c8634a
BintiSholikhah <bintisholikhah95@gmail.com>
2022-06-15 14:50:39 +0700

    konversi ke matriks 2 dimensi

--- studi kasus 9.cpp
+++ studi kasus 9.cpp
@@ -25,3 +25,39 @@ void matriks::input(){
 }
 
 
+
+
+
+
+void matriks::konversi(){
+  int i=0;
+  cout << "Masukkan banyak kolom: ";
+  cin >> kolom;
+  cout << "Masukkan banyak baris: ";
+  cin >> baris;
+  for(int k=0; k<baris; k++){
+  	for(int l=0; l<kolom; l++){
+  		data2[k][l] = data[i];
+  		i++;
+	  }
+  }
+  cout << endl;
+  for(int k=0; k<baris; k++){
+  	for(int l=0; l<kolom; l++){
+  		cout << data2[k][l] << " ";
+	  }
+	  cout << endl;
+	}
+}
+int main(){
+  matriks Sarah;
+  Sarah.input();
+  cout << endl << endl;
+  cout << "Hasil sorting: ";
+  Sarah.penyortiran();
+  cout << endl << endl;
+  Sarah.konversi();
+  return 0;
+}
+
+
